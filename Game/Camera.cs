c834212bf83ccc7c702c6354d2b1dc3b4425a3c4
Yuen <1044001101@qq.com>
2022-02-22 09:58:11 +0800

    [Init]: First Commit | 第一次提交
    -FrameWork-
    1. Add Class "DrawManager" for the Management of Draw-Relating Methods
    2. Add Class "Animation" for the Storage of Animation Resources
    
    3. Add Unmodified Class Files: Sprite GameObject, Entity, Enemy, Npc, Player, Projectile
    4. Add Unmodified Class File: "TextureSet"
    
    -Resources-
    5. Add Image & Json File for [Bouncing Boob]

---
+++ Game/Camera.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xna = Microsoft.Xna.Framework;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+
+namespace Mythonia.Game
+{
+    class Camera
+    {
+        Vector2 Position;
+        float ZoomRate;
+
+        public Camera(float x = 0 , float y = 0, float zoomRate = 1f)
+        {
+            Position = new Vector2(x, y);
+            ZoomRate = zoomRate;
+        }
+    }
+}
---
+++ Game/Objects/GameObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mythonia.Game.Objects
+{
+    abstract class GameObject
+    {
+        
+
+    }
+}
